1df90cd57a67577279ed99b5a178f592e33da564
Kauã Henrique <kaua.henrique07@hotmail.com>
2023-06-09 22:32:56 -0300

    criada as validações de cadastro_nota

--- StudentTracker/app/src/main/java/br/unipar/studenttracker/activitys/Cadastro_Notas_Activitys.java
+++ StudentTracker/app/src/main/java/br/unipar/studenttracker/activitys/Cadastro_Notas_Activitys.java
@@ -1,24 +1,66 @@
 package br.unipar.studenttracker.activitys;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
-import java.util.ArrayList;
-
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 import br.unipar.studenttracker.R;
-import br.unipar.studenttracker.globais.Globais;
 
 public class Cadastro_Notas_Activitys extends AppCompatActivity {
+
+    private EditText editRa;
+    private EditText editNome;
+    private Spinner selectDisciplina;
+    private EditText editNota;
+    private Spinner selectBimestre;
+    private Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro_notas);
 
-        if (Globais.listaAluno == null) {
-            Globais.listaAluno = new ArrayList<>();
+        editRa = findViewById(R.id.edit_ra);
+        editNome = findViewById(R.id.edit_nome);
+        selectDisciplina = findViewById(R.id.select_disciplina);
+        editNota = findViewById(R.id.edita_nota);
+        selectBimestre = findViewById(R.id.select_bimestre);
+        btnAdd = findViewById(R.id.btn_add);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adicionarNota();
+            }
+        });
+    }
+
+    private void adicionarNota() {
+        String ra = editRa.getText().toString().trim();
+        String nome = editNome.getText().toString().trim();
+        String disciplina = selectDisciplina.getSelectedItem().toString().trim();
+        String nota = editNota.getText().toString().trim();
+        String bimestre = selectBimestre.getSelectedItem().toString().trim();
+
+        if (TextUtils.isEmpty(ra) || TextUtils.isEmpty(nome) || TextUtils.isEmpty(disciplina)
+                || TextUtils.isEmpty(nota) || TextUtils.isEmpty(bimestre)) {
+            Toast.makeText(this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show();
+            return;
         }
-        
+
+        String mensagem = "Nota cadastrada com sucesso!";
+
+        Toast.makeText(this, mensagem, Toast.LENGTH_SHORT).show();
+
+        // Limpar os campos após adicionar a nota
+        editRa.setText("");
+        editNome.setText("");
+        selectDisciplina.setSelection(0);
+        editNota.setText("");
+        selectBimestre.setSelection(0);
     }
 }
--- StudentTracker/app/src/main/java/br/unipar/studenttracker/model/Aluno.java
+++ StudentTracker/app/src/main/java/br/unipar/studenttracker/model/Aluno.java
@@ -1,4 +1,70 @@
 package br.unipar.studenttracker.model;
 
 public class Aluno {
+
+    private int ra;
+    private String nome;
+    private String disciplina;
+    private double nota;
+    private int bimestre;
+
+    public Aluno(int ra, String nome, String disciplina, double nota, int bimestre) {
+        this.ra = ra;
+        this.nome = nome;
+        this.disciplina = disciplina;
+        this.nota = nota;
+        this.bimestre = bimestre;
+    }
+
+    public int getRa() {
+        return ra;
+    }
+
+    public void setRa(int ra) {
+        this.ra = ra;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public int getBimestre() {
+        return bimestre;
+    }
+
+    public void setBimestre(int bimestre) {
+        this.bimestre = bimestre;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "ra=" + ra +
+                ", nome='" + nome + '\'' +
+                ", disciplina='" + disciplina + '\'' +
+                ", nota=" + nota +
+                ", bimestre=" + bimestre +
+                '}';
+    }
 }
+
